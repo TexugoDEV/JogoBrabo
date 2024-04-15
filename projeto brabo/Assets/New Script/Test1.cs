6df363036016957686830063e8904a2b3482307d
Texugo_DEV <89714547+TexugoDEV@users.noreply.github.com>
2024-04-15 19:35:05 -0300

    update

---
+++ projeto brabo/Assets/New Script/Test1.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int count = 1;
+        do
+        {
+            Debug.Log(count);
+            count++;
+
+        }while (count <= 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ projeto brabo/Assets/New Script/Test2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string[] fruits = new string[]
+        {
+            "apple","banana","cherry","orange"
+        };
+
+        foreach (string fruit in fruits)
+        {
+            Debug.Log(fruit);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
