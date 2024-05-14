c9871b5b62d75f09357f8977c42843d4853f84aa
Texugo_DEV <89714547+TexugoDEV@users.noreply.github.com>
2024-05-14 19:43:58 -0300

    Update

---
+++ projeto brabo/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform WaypointA;
+    public Transform WaypointB;
+    public float moveSpeed = 3f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
